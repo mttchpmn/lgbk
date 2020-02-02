ebbd3599971f9649e4960f5aa2710374d6a6b3c4
Matt Chapman <matt@mttchpmn.com>
2020-02-02 23:02:43 +1300

    Update landing page

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-const Landing = () => <div>Landing</div>;
+import { Box, Typography } from "@material-ui/core";
+
+const Landing = () => (
+  <Box style={{ marginTop: 200 }}>
+    <Typography variant="h1">lgbk</Typography>
+    <Typography variant="subtitle1">
+      Minimalist journaling for a busy mind
+    </Typography>
+  </Box>
+);
 
 export default Landing;
